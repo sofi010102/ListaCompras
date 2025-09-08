7594d731964afa8855a6a933750536d396193016
Sofiz <sofia.romano@aluno.ifsp.edu.br>
2025-09-08 20:15:22 -0300

    Delete scripts. directory

--- scripts./verificarListaVazia.js
+++
@@ -1,8 +0,0 @@
-const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
-
-function verificarListaVazia(lista) {
-    const itens = lista.querySelectorAll("li");
-    mensagemListaVazia.style.display = itens.length === 0 ? "block" : "none";
-}
-
-export default verificarListaVazia;
