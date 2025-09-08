a475559db5748cbbc6bfe2daa9be47e7ce43746d
Sofiz <sofia.romano@aluno.ifsp.edu.br>
2025-09-08 18:08:21 -0300

    Delete verificarListaVazia.js

--- verificarListaVazia.js
+++ verificarListaVazia.js
@@ -1,8 +1,1 @@
-const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
 
-function verificarListaVazia(listaDeCompras) {
-    const itensDaLista = listaDeCompras.querySelectorAll("li");
-    mensagemListaVazia.style.display = itensDaLista.length === 0 ? "block" : "none";
-}
-
-export default verificarListaVazia;
