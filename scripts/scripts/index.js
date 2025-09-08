cc6dd830a4bc4663cf0308a206987930fc524a2c
Sofiz <sofia.romano@aluno.ifsp.edu.br>
2025-09-08 18:04:37 -0300

    Create index.js

---
+++ scripts/scripts/index.js
@@ -0,0 +1,21 @@
+import { criarItemDaLista } from "./criarItemDaLista.js";
+import verificarListaVazia from "./verificarListaVazia.js";
+
+const inputItem = document.getElementById("input-item");
+const listaDeCompras = document.getElementById("lista-de-compras");
+const botaoAdicionar = document.getElementById("adicionar-item");
+botaoAdicionar.style.cursor = "pointer";
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+
+    const itemDaLista = criarItemDaLista();
+    if (!itemDaLista) return;
+
+    listaDeCompras.appendChild(itemDaLista);
+    verificarListaVazia(listaDeCompras);
+
+    inputItem.value = "";
+});
+
+verificarListaVazia(listaDeCompras);
