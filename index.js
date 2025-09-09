79fa797d2c2b52de808803961ea4212fb3d2855c
Sofiz <sofia.romano@aluno.ifsp.edu.br>
2025-09-08 22:49:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const inputItem = document.getElementById("input-item");
 const listaDeCompras = document.getElementById("lista-de-compras");
 const botaoAdicionar = document.getElementById("adicionar-item");
 
-botaoAdicionar.addEventListener("click", (e) => {
-    e.preventDefault();
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
     const itemDaLista = criarItemDaLista();
-    if (!itemDaLista) return;
-    listaDeCompras.appendChild(itemDaLista);
-    verificarListaVazia(listaDeCompras);
-    inputItem.value = "";
+    if (itemDaLista) {
+        listaDeCompras.appendChild(itemDaLista);
+        verificarListaVazia(listaDeCompras);
+        inputItem.value = "";
+    }
 });
 
 verificarListaVazia(listaDeCompras);
